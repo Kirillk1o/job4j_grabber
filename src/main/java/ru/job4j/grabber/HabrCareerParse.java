8963ea8a6db0be4e715baaec2a9717fd1fc54287
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-12-09 23:00:40 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,49 +1,69 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    private String retrieveDescription(String link) throws IOException {
-        Document document = Jsoup.connect(link).get();
-        Element descriptionElement = document.selectFirst(".style-ugc");
-        return descriptionElement.text();
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    public static void main(String[] args) throws IOException {
+    /*
+    метод парсит 5 страниц
+    c названием вакансии, ссылкой на вакансию, описанием и датой размещения.
+     */
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(link + "?page=" + i).get();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            /*
+            получаем все вакансии страницы
+             */
+            Elements rows = doc.select(".vacancy-card__inner");
             rows.forEach(row -> {
                 Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
-                String vacancyDate = row.select(".basic-date").first().attr("datetime");
-                LocalDateTime localDateTime = new HabrCareerDateTimeParser().parse(vacancyDate);
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String description = null;
-                try {
-                    description = new HabrCareerParse().retrieveDescription(link);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.printf("%s %s %s %s%n", vacancyName, link, description, localDateTime);
+                String linkString = String.format("%s%s", SOURCE_LINK, titleElement.attr("href"));
+                String description = retrieveDescription(linkString);
+                String vacancyDate = row.select(".bacic-date").first().attr("datetime");
+                list.add(new Post(vacancyName, linkString, description, this.dateTimeParser.parse(vacancyDate)));
             });
         }
+        return list;
+    }
+
+    /*
+    метод для загрузки деталей объявления (полное описание)
+     */
+    private String retrieveDescription(String link) {
+        Document document = null;
+        try {
+            document = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Elements descriptionElement = document.select(".style-ugc");
+        return descriptionElement.text();
     }
 }
