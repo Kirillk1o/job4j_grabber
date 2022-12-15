6b688ead10189b5debc56e9788f322200947e620
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-12-15 19:13:13 +0300

    1. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,9 +1,6 @@
 package ru.job4j.kiss;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class MaxMin {
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -8,21 +8,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MaxMinTest {
-
-    private Comparator<Integer> comparator;
-
-    @BeforeEach
-    void test() {
-        comparator = Comparator.naturalOrder();
-    }
 
     @Test
     void whenFindMax() {
         MaxMin max = new MaxMin();
         List<Integer> list = Arrays.asList(12, 11, 25, 2, 9, 1);
+        Comparator<Integer> comparator = Integer::compareTo;
         assertThat(max.max(list, comparator)).isEqualTo(25);
     }
 
@@ -30,6 +22,23 @@ class MaxMinTest {
     void whenFindMin() {
         MaxMin min = new MaxMin();
         List<Integer> list = Arrays.asList(12, 11, 25, 2, 9, 1);
+        Comparator<Integer> comparator = Integer::compareTo;
         assertThat(min.min(list, comparator)).isEqualTo(1);
+    }
+
+    @Test
+    void whenFindMaxString() {
+        MaxMin max = new MaxMin();
+        List<String> list = Arrays.asList("privet", "poka", "Gabe Nuel", "Jakarta");
+        Comparator<String> comparator = String::compareTo;
+        assertThat(max.max(list, comparator)).isEqualTo("privet");
+    }
+
+    @Test
+    void whenFindMinString() {
+        MaxMin min = new MaxMin();
+        List<String> list = Arrays.asList("privet", "poka", "Gabe Nuel", "Jakarta");
+        Comparator<String> comparator = String::compareTo;
+        assertThat(min.min(list, comparator)).isEqualTo("Gabe Nuel");
     }
 }
